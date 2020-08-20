7c3c885ba899fcc164890c6ef718e3f78cba7be9
Clemilton01 <62109370+Clemilton01@users.noreply.github.com>
2020-08-20 11:14:00 -0300

    Update exemple.py

--- LTP2/Pratica01/exemple.py
+++ LTP2/Pratica01/exemple.py
@@ -1,3 +1,3 @@
 valor_cm=int(input("informe um valor em cm: "))
 valor_m= valor_cm / 100
-print("o valor", valor_cm,"em metros é igual a:", valor_m)
+print("o valor", valor_cm,"em metros é igual a", valor_m,"metros")
