792e75b1f54261fd808d428c7e899f9c08ba9faf
Clemilton01 <62109370+Clemilton01@users.noreply.github.com>
2020-08-27 11:01:02 -0300

    Create programa04.py

---
+++ LTP2/Prática02/programa04.py
@@ -0,0 +1,10 @@
+numero = int(input("mumero: "))
+resto = numero % 2
+if resto == 0:
+  print("esse é par bobão")
+  print("shinra tensei")
+
+else:
+  print("esse é impar bobão")
+
+  print("never desista dos seus dreams")
