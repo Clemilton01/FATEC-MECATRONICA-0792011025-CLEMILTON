ee5f166ee9be4ecc107affbdb3a8f0b150b20f7f
Clemilton01 <62109370+Clemilton01@users.noreply.github.com>
2020-12-03 11:17:00 -0300

    Create codigo.py

---
+++ LTP2/Projeto LTP2/codigo.py
@@ -0,0 +1,153 @@
+# A very simple Flask Hello World app for you to get started with...
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {font-size:40px; text-align:center;}
+
+.monospace {
+  font-family: "Lucida Console", Courier, monospace;
+
+a:visited {
+  color: white;
+  padding: 0px 0px;
+  text-align: center;
+  text-decoration: underline;
+  display: inline-block;
+}
+
+
+.serif {
+  font-family: "Times New Roman", Times, serif;
+}
+</style>
+</head>
+<body>
+<head>
+    <title>PlayStation 5</title>
+    <h1 class="monospace">PLAYSTATION 5</h1>
+</head>
+<style>
+body {
+  background-image: url('https://miro.medium.com/max/3840/1*VMhvD51Nm2P6CBRViTFyxQ.jpeg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th, tr {
+  border: 2px solid #404040;
+  text-align: left;
+  padding: 7px;
+}
+td{
+    background-color: #eee;
+}
+
+</style>
+<body>
+    <h2><font face="Helvetica"><center> Seja bem vindo a nova era dos games! Bem vindo ao PlayStation 5!
+ </font> <br /></h2></center>
+
+
+    <p style="font-size:170%;"><font face="Helvetica">Sony PlayStation 5 (PS5) — Ficha Técnica</font></p>
+
+    <table>
+  <tr>
+    <td>Processador:</td>
+    <th>Octa-core AMD de arquitetura Zen 2 (7 nm), com clock de até 3,5 GHz</th>
+
+  </tr>
+  <tr>
+    <td>Chip gráfico:</td>
+    <th>GPU customizada RDNA 2 com 36 unidades computacionais, clock de até 2,23 GHz e 10,28 Tflop/s</th>
+
+  </tr>
+  <tr>
+    <td>Memória RAM:</td>
+    <th>16 GB GDDR6</th>
+
+  </tr>
+  <tr>
+    <td>Largura de banda da RAM:</td>
+    <th>448 GB/s</th>
+
+  </tr>
+  <tr>
+    <td>Taxa de transferência da RAM:</td>
+    <th>14 GB/s</th>
+
+  </tr>
+  <tr>
+    <td>Armazenamento interno:</td>
+    <th>SSD customizado de 825 GB, barramento de 12 canais e controlador PCI Express 4.0, com taxa de transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s (compactado)</th>
+
+  </tr>
+  <tr>
+    <td>Armazenamento expansível:</td>
+    <th>Compatível com SSDs NVMe</th>
+
+  </tr>
+  <tr>
+    <td>Armazenamento externo:</td>
+    <th>Suporte a unidades externas via porta USB</th>
+
+  </tr>
+  <tr>
+    <td>Drive óptico:</td>
+    <th>Leitor de discos Blu-ray 4K UHD</th>
+
+  </tr>
+  <tr>
+    <td>Reprodução de gráficos:</td>
+    <th>Suporte a ray tracing</th>
+
+  </tr>
+
+
+
+</body>
+
+<style>
+.footer {
+  position: fixed;
+  font: monospace;
+  font-size: 15px;
+  left: 0;
+  bottom: 0;
+  background-image:url('https://observatoriodegames.uol.com.br/wp-content/uploads/2020/07/PS5.png') ;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  background-color: white;
+  color: white;
+  text-align: center;
+}
+<body>
+  <h1><font face="Helvetica"><center> Jogar Não Tem Limites!
+ </font> <br /></h1></center>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/RkC0l4iekYo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <body>
+  <h1>font face="halvetica"><center> Recursos do PS5
+  </font> <br /></h1></center>
+   <h2><font face="Helvetica"><center> SSD ultrarrápido
+ </font> <br /></h2></center>
+ <p> Maximize suas sessões de jogo com tempos de carregamento praticamente instantâneos para jogos do PS5™ instalados. </p>
+  <h2><font face="Helvetica"><center> E/S integrada
+ </font> <br /></h2></center>
+<p> A integração personalizada dos sistemas de console PS5 permite que os criadores extraiam dados do SSD tão rápido que eles podem desenvolver jogos de formas que antes seriam impossíveis.</p>
+
+</style>
+
+<div class="footer">
+  <p>PlayStation®5, o maior console da Sony, em suas mãos! Veja <a href="https://www.techtudo.com.br/noticias/2020/09/pre-venda-do-ps5-no-brasil-veja-precos-e-lojas-com-console-disponivel.ghtml">onde comprar</a>. Confira também todos os
+      <a href="https://www.copeltelecom.com/site/blog/playstation-5-conheca-os-primeiros-jogos-exclusivos/">jogos exclusivos</a>.</p>
+
+
+</div>
